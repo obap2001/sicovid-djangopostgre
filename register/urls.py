9382c5adb3d0e8f688414137e01323519580dc65
Gabriel Erwhian Winarso <gabrielerwhian@gmail.com>
2021-05-31 09:48:44 +0700

    Added url link for register pengguna publik

--- register/urls.py
+++ register/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import registerAdmin
+from .views import registerAdmin, registerPenggunaPublik
 
 urlpatterns = [
-    path('admin/', registerAdmin, name='register_admin')
+    path('admin/', registerAdmin, name='register_admin'),
+    path('penggunapublik/', registerPenggunaPublik, name='register_pengguna_publik')
 ]
