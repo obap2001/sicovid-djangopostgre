c431e0222498f35c57ad7abe2617520f7cb6694b
Gabriel Erwhian Winarso <gabrielerwhian@gmail.com>
2021-05-31 09:29:54 +0700

    Updated register admin to handle exception

--- register/views.py
+++ register/views.py
@@ -1,26 +1,50 @@
 from django.http import response
+from django.contrib import messages
 from django.shortcuts import render
-from django.db import connection
+from django.db import connection, InternalError
 from .forms import adminRegisterForm, penggunaPublikRegisterForm
 
-# Create your views here.
+# Register Admin
 def registerAdmin(request):
+    # response for Passing data into Templates
     response = {}
+
+    # Institiate The Form
     form = adminRegisterForm(request.POST)
+
+    # Assigning data for response
     response['form'] = form
+
+    # Getting data from Form on POST
     if request.method == 'POST' and form.is_valid():
         email = form.cleaned_data['email']
         password = form.cleaned_data['password']
 
-        # Execute Query
+        # Execute Query for Registering
         with connection.cursor() as cursor:
-            cursor.execute(
-                f'''set search_path to siruco; 
-                insert into akun_pengguna values
-                ('{email}','{password}' ,'Admin Sistem');
-                insert into admin values
-                ('{email}');'''
-                )
+            try:
+                cursor.execute(
+                    f'''set search_path to siruco; 
+                    insert into akun_pengguna values
+                    ('{email}','{password}' ,'Admin Sistem');
+                    insert into admin values
+                    ('{email}');'''
+                    )
+                messages.success(request,f'Successfully Registerd, Welcome {email}')
+
+            except InternalError:
+                # Check if user already regitstered or not
+                cursor.execute(
+                    f'''set search_path to siruco;
+                    SELECT USERNAME FROM AKUN_PENGGUNA
+                    WHERE USERNAME='{email}';'''
+                    )
+                data = cursor.fetchone()
+                if data: # Exception if user already register
+                    messages.error(request,'User has already been registered')
+                else: # Exception if password does not match requirement
+                    messages.error(request,'Password must have at least 1 capital letters and 1 number')
+
     return render(request,'register.html',response)
 
 def registerPenggunaPublik(request):
@@ -42,4 +66,4 @@ def registerPenggunaPublik(request):
                     ('{username}', '{nik}', '{nama}', '{status}', 'PENGGUNA PUBLIK', '{noHP}');
                 '''
             )
-    return render (request, 'register.html',response)
+    return render(request, 'register.html',response)
