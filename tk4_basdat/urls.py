8e75f2a77a8dbb3e26b4773d3a96438f857af5b7
Gabriel Erwhian Winarso <gabrielerwhian@gmail.com>
2021-06-01 18:09:18 +0700

    Added placeholder home

--- tk4_basdat/urls.py
+++ tk4_basdat/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-
+from base_html.views import home_view
 
 urlpatterns = [
     path('register/', include('register.urls')),
+    path('', home_view, name='home')
 ]
