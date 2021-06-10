df680b53823fa1a86de70b17a3a642b78d0bd53e
Gabriel Erwhian Winarso <gabrielerwhian@gmail.com>
2021-06-10 19:40:45 +0700

    Implemented List Feature

--- reservasi_rumah_sakit/forms.py
+++ reservasi_rumah_sakit/forms.py
@@ -1,14 +1,85 @@
+from django.core.exceptions import ValidationError
+from tk4_basdat.settings import DATE_INPUT_FORMATS
 from django import forms
 from django.db import connection
 
-def fetch_data_faskes():
+def fetch_data_rs():
     faskes_data_choice = []
     with connection.cursor() as cursor:
         cursor.execute(
-        'SELECT nik, Nama From NIK;'
+        'SELECT kode_faskes From RUMAH_SAKIT;'
         )
         faskes_data_choice = cursor.fetchall() #Will Get all output of the query
-    return tuple(faskes_data_choice)
+
+    # Organized the data
+    data_organized = []
+    for i in faskes_data_choice:
+        temp = (i[0],i[0])
+        data_organized.append(temp)
+
+    return tuple(data_organized)
+
+def fetch_data_ruangan_rs():
+    faskes_data_choice = []
+    with connection.cursor() as cursor:
+        cursor.execute(
+        'SELECT koders From RUANGAN_RS;'
+        )
+        faskes_data_choice = cursor.fetchall() #Will Get all output of the query
+
+    # Organized the data
+    data_organized = []
+    for i in faskes_data_choice:
+        temp = (i[0],i[0])
+        data_organized.append(temp)
+
+    return tuple(data_organized)
+
+def fetch_data_bed_rs():
+    faskes_data_choice = []
+    with connection.cursor() as cursor:
+        cursor.execute(
+        'SELECT KODEBED From BED_RS;'
+        )
+        faskes_data_choice = cursor.fetchall() #Will Get all output of the query
+
+    # Organized the data
+    data_organized = []
+    for i in faskes_data_choice:
+        temp = (i[0],i[0])
+        data_organized.append(temp)
+    return tuple(data_organized)
 
 class CreateReservasiForm(forms.Form):
-    Nik = forms.ChoiceField(label='NIK')
+    nik = forms.ChoiceField(label='NIK Pasien',required = True)
+    tanggal_masuk = forms.DateField(label="Tanggal Masuk",required=True, input_formats=DATE_INPUT_FORMATS)
+    tanggal_keluar = forms.DateField(label="Tanggal Keluar",required=True, input_formats=DATE_INPUT_FORMATS)
+    kode_rumah_sakit = forms.ChoiceField(label="Kode Rumah Sakit", choices=fetch_data_ruangan_rs(), required=True)
+    kode_ruangan = forms.ChoiceField(label="Kode Ruangan", choices=(), required=True)
+    kode_bed = forms.ChoiceField(label="Kode Bed", choices=(), required=True)
+
+    def clean(self):
+        form_data = self.cleaned_data
+        if form_data['tanggal_keluar'] <= form_data['tanggal_masuk']:
+            raise ValidationError('Tanggal Keluar tidak boleh kurang dari Tanggal_masuk')
+
+class UpdateReservasiForm(forms.Form):
+    nik = forms.ChoiceField(disabled=True, label='NIK Pasien',required = True)
+    tanggal_masuk = forms.DateField(disabled=True,label="Tanggal Masuk",required=True, input_formats=DATE_INPUT_FORMATS)
+    tanggal_keluar = forms.DateField(label="Tanggal Keluar",required=True, input_formats=DATE_INPUT_FORMATS)
+    kode_rumah_sakit = forms.ChoiceField(disabled=True,label="Kode Rumah Sakit", choices=fetch_data_ruangan_rs(), required=True)
+    kode_ruangan = forms.ChoiceField(disabled=True,label="Kode Ruangan", choices=fetch_data_ruangan_rs, required=True)
+    kode_bed = forms.ChoiceField(disabled=True,label="Kode Bed", choices=fetch_data_bed_rs, required=True)
+
+class DetailReservasiForm(forms.Form):
+    nik = forms.ChoiceField(disabled=True, label='NIK Pasien',required = True)
+    tanggal_masuk = forms.DateField(disabled=True, label="Tanggal Masuk", required=True, input_formats=DATE_INPUT_FORMATS)
+    tanggal_keluar = forms.DateField(disabled=True, label="Tanggal Keluar", required=True, input_formats=DATE_INPUT_FORMATS)
+    kode_rumah_sakit = forms.ChoiceField(disabled=True, label="Kode Rumah Sakit", choices=fetch_data_ruangan_rs(), required=True)
+    kode_ruangan = forms.ChoiceField(disabled=True, label="Kode Ruangan", choices=fetch_data_ruangan_rs, required=True)
+    kode_bed = forms.ChoiceField(disabled=True, label="Kode Bed", choices=fetch_data_bed_rs, required=True)
+
+
+
+
+
--- reservasi_rumah_sakit/views.py
+++ reservasi_rumah_sakit/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.db import connection
+from .forms import CreateReservasiForm, UpdateReservasiForm, DetailReservasiForm
 
 # Create your views here.
+def create_reservasi_rs_view(request):
+    pass
+
+def list_reservasi_rs_view(request):
+    if 'username' in request.session and (request.session['peran'] == 'ADMIN_SATGAS' or request.session['peran'] == 'PENGGUNA_PUBLIK' ):
+        response = {}
+        data_reservasi = []
+
+        # Fetch Data
+        with connection.cursor() as cursor:
+            cursor.execute(f'''
+            SELECT * FROM RESERVASI_RS;
+            ''')
+            data_reservasi = cursor.fetchall()
+
+        # Reorganized Data
+        id_now = 1
+        data_organized = []
+        for i in data_reservasi:
+            temp = (id_now, i[0],i[1].strftime('%d-%m-%Y'), i[2].strftime('%d-%m-%Y'),i[3],i[4],i[5] )
+            data_organized.append(temp)
+            id_now += 1
+        response['data_reservasi'] = data_organized
+
+        return render(request,'list_reservasi_rs.html',response)
+
+    else:
+        return render('home')
+
+def detail_reservasi_rs_view(request,kode_pasien,tanggal):
+    pass
+
+def update_reservasi_rs_view(request,kode_pasien,tanggal):
+    pass
+
+def delete_reservasi_rs_view(request,kode_pasien,tanggal):
+    pass
+
+
+
+def fetch_data_ruangan(request):
+    rs = request.GET.get('rumah_sakit')
+    list_ruangan = []
+
+    with connection.cursor() as cursor:
+        cursor.execute(f'''
+            SELECT koderuangan FROM RUANGAN_RS
+            WHERE koders='{rs}';
+        ''')
+        list_ruangan = cursor.fetchall()
+
+    # Organized the data
+    cleaned_data = []
+    for i in list_ruangan:
+        temp= (i[0],i[0])
+        cleaned_data.append(temp)
+    return render(request, 'ruangan_dropdown.html', {'list_ruangan': cleaned_data})
+
