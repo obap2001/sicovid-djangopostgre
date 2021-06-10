c7ede56965eec208f9a6f030067b5da2ca0cebf7
Gabriel Erwhian Winarso <gabrielerwhian@gmail.com>
2021-06-10 21:00:01 +0700

    Implemented Update and delete feature

--- reservasi_rumah_sakit/forms.py
+++ reservasi_rumah_sakit/forms.py
@@ -19,11 +19,11 @@ def fetch_data_rs():
 
     return tuple(data_organized)
 
-def fetch_data_ruangan_rs():
+def fetch_nik_pasien():
     faskes_data_choice = []
     with connection.cursor() as cursor:
         cursor.execute(
-        'SELECT koders From RUANGAN_RS;'
+        'SELECT NIK From PASIEN;'
         )
         faskes_data_choice = cursor.fetchall() #Will Get all output of the query
 
@@ -33,6 +33,22 @@ def fetch_data_ruangan_rs():
         temp = (i[0],i[0])
         data_organized.append(temp)
 
+    return tuple(data_organized)
+
+
+def fetch_data_ruangan_rs():
+    faskes_data_choice = []
+    with connection.cursor() as cursor:
+        cursor.execute(
+        'SELECT koderuangan From RUANGAN_RS;'
+        )
+        faskes_data_choice = cursor.fetchall() #Will Get all output of the query
+
+    # Organized the data
+    data_organized = []
+    for i in faskes_data_choice:
+        temp = (i[0],i[0])
+        data_organized.append(temp)
     return tuple(data_organized)
 
 def fetch_data_bed_rs():
@@ -51,10 +67,10 @@ def fetch_data_bed_rs():
     return tuple(data_organized)
 
 class CreateReservasiForm(forms.Form):
-    nik = forms.ChoiceField(label='NIK Pasien',required = True)
+    nik = forms.ChoiceField(label='NIK Pasien',choices=fetch_nik_pasien(),required = True)
     tanggal_masuk = forms.DateField(label="Tanggal Masuk",required=True, input_formats=DATE_INPUT_FORMATS)
     tanggal_keluar = forms.DateField(label="Tanggal Keluar",required=True, input_formats=DATE_INPUT_FORMATS)
-    kode_rumah_sakit = forms.ChoiceField(label="Kode Rumah Sakit", choices=fetch_data_ruangan_rs(), required=True)
+    kode_rumah_sakit = forms.ChoiceField(label="Kode Rumah Sakit", choices=fetch_data_rs(), required=True)
     kode_ruangan = forms.ChoiceField(label="Kode Ruangan", choices=(), required=True)
     kode_bed = forms.ChoiceField(label="Kode Bed", choices=(), required=True)
 
@@ -64,20 +80,13 @@ class CreateReservasiForm(forms.Form):
             raise ValidationError('Tanggal Keluar tidak boleh kurang dari Tanggal_masuk')
 
 class UpdateReservasiForm(forms.Form):
-    nik = forms.ChoiceField(disabled=True, label='NIK Pasien',required = True)
+    nik = forms.ChoiceField(disabled=True, label='NIK Pasien',choices=fetch_nik_pasien(),required = True)
     tanggal_masuk = forms.DateField(disabled=True,label="Tanggal Masuk",required=True, input_formats=DATE_INPUT_FORMATS)
     tanggal_keluar = forms.DateField(label="Tanggal Keluar",required=True, input_formats=DATE_INPUT_FORMATS)
-    kode_rumah_sakit = forms.ChoiceField(disabled=True,label="Kode Rumah Sakit", choices=fetch_data_ruangan_rs(), required=True)
-    kode_ruangan = forms.ChoiceField(disabled=True,label="Kode Ruangan", choices=fetch_data_ruangan_rs, required=True)
-    kode_bed = forms.ChoiceField(disabled=True,label="Kode Bed", choices=fetch_data_bed_rs, required=True)
+    kode_rumah_sakit = forms.ChoiceField(disabled=True,label="Kode Rumah Sakit", choices=fetch_data_rs(), required=True)
+    kode_ruangan = forms.ChoiceField(disabled=False,label="Kode Ruangan", choices=fetch_data_ruangan_rs(), required=True)
+    kode_bed = forms.ChoiceField(disabled=True,label="Kode Bed", choices=fetch_data_bed_rs(), required=True)
 
-class DetailReservasiForm(forms.Form):
-    nik = forms.ChoiceField(disabled=True, label='NIK Pasien',required = True)
-    tanggal_masuk = forms.DateField(disabled=True, label="Tanggal Masuk", required=True, input_formats=DATE_INPUT_FORMATS)
-    tanggal_keluar = forms.DateField(disabled=True, label="Tanggal Keluar", required=True, input_formats=DATE_INPUT_FORMATS)
-    kode_rumah_sakit = forms.ChoiceField(disabled=True, label="Kode Rumah Sakit", choices=fetch_data_ruangan_rs(), required=True)
-    kode_ruangan = forms.ChoiceField(disabled=True, label="Kode Ruangan", choices=fetch_data_ruangan_rs, required=True)
-    kode_bed = forms.ChoiceField(disabled=True, label="Kode Bed", choices=fetch_data_bed_rs, required=True)
 
 
 
--- reservasi_rumah_sakit/views.py
+++ reservasi_rumah_sakit/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.db import connection
-from .forms import CreateReservasiForm, UpdateReservasiForm, DetailReservasiForm
+from django.contrib import messages
+from .forms import CreateReservasiForm, UpdateReservasiForm
 
 # Create your views here.
 def create_reservasi_rs_view(request):
@@ -32,15 +33,69 @@ def list_reservasi_rs_view(request):
     else:
         return render('home')
 
-def detail_reservasi_rs_view(request,kode_pasien,tanggal):
-    pass
-
 def update_reservasi_rs_view(request,kode_pasien,tanggal):
-    pass
+    if 'username' in request.session and request.session['peran'] == 'ADMIN_SATGAS':
+        response = {}
+        data_reservasi = {} #Init data_pasien
+        tanggal_splitted = tanggal.split('-')
+
+        # Fetch Data Reservasi
+        with connection.cursor() as cursor:
+            cursor.execute(f'''
+            SELECT * FROM RESERVASI_RS WHERE
+            KODEPASIEN = '{kode_pasien}' AND
+            tglmasuk='{tanggal_splitted[2]}-{tanggal_splitted[1]}-{tanggal_splitted[0]}';
+            ''')
+            data_reservasi = cursor.fetchone()
+
+        init_reservasi= {
+            'nik': data_reservasi[0],
+            'tanggal_masuk': data_reservasi[1],
+            'tanggal_keluar': data_reservasi[2],
+            'kode_rumah_sakit': data_reservasi[3],
+            'kode_ruangan': data_reservasi[4],
+            'kode_bed': data_reservasi[5]
+        }
+
+        form_reservasi = UpdateReservasiForm(request.POST or None, initial=init_reservasi)
+        response['form_reservasi'] = form_reservasi
+
+        if request.method == 'POST' and form_reservasi.is_valid():
+            tanggal_keluar = form_reservasi.cleaned_data['tanggal_keluar']
+
+            with connection.cursor() as cursor:
+                    cursor.execute(f'''
+                        UPDATE RESERVASI_RS
+                        SET tglkeluar = '{tanggal_keluar.strftime('%Y-%m-%d')}'
+                        WHERE kodepasien = '{kode_pasien}' and
+                        tglmasuk='{tanggal_splitted[2]}-{tanggal_splitted[1]}-{tanggal_splitted[0]}' ;
+                    ''')
+
+            messages.success(request, 'Data Reservasi Behasil diubah')
+            return redirect('list_reservasi')
+
+        return render(request,'update_reservasi_rs.html',response)
+
+    else:
+        return render('home')
 
 def delete_reservasi_rs_view(request,kode_pasien,tanggal):
-    pass
+    if 'username' in request.session and request.session['peran'] == 'ADMIN_SATGAS':
+        # Deleting data in SQL
+        tanggal_splitted = tanggal.split('-')
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f'''
+                DELETE FROM RESERVASI_RS
+                WHERE kodepasien = '{kode_pasien}' and
+                tglmasuk='{tanggal_splitted[2]}-{tanggal_splitted[1]}-{tanggal_splitted[0]}';
+                '''
+            )
+        messages.success(request, f'Data Reservasi berhasil dihapus')
+        return redirect('list_reservasi')
 
+    else:
+        return render('home')
 
 
 def fetch_data_ruangan(request):
