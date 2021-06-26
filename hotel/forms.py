ba5e8d256ab4bf1c1e74b578bdebd57457a134a3
oba20201 <obapakpahan01@gmail.com>
2021-06-26 11:46:01 +0700

    fix CRU Hotel (No.18)

--- hotel/forms.py
+++ hotel/forms.py
@@ -4,7 +4,7 @@ from django.db import connection
 class CreateHotelForm(forms.Form):
     kode = forms.CharField(max_length=5, disabled=True)
     namaHotel = forms.CharField(max_length=30, required=True)
-    rujukan = forms.BooleanField()
+    rujukan = forms.BooleanField(required=False)
 
 class CreateAlamatHotelForm(forms.Form):
     jalan = forms.CharField(label='Jalan' ,max_length=30, required=True)
@@ -12,3 +12,15 @@ class CreateAlamatHotelForm(forms.Form):
     kecamatan = forms.CharField(label='Kecamatan',max_length=30, required=True)
     kabupaten_kota = forms.CharField(label='Kabupaten/Kota',max_length=30, required=True)
     provinsi = forms.CharField(label='Provinsi', max_length=30, required=True) 
+
+class FormUpdateHotel(forms.Form):
+    kode = forms.CharField(max_length=5, disabled=True)
+    nama = forms.CharField(max_length=30, required=True)
+    rujukan = forms.BooleanField(required=False)
+
+class FormUpdateAlamatHotel(forms.Form):
+    jalan = forms.CharField(label='Jalan' ,max_length=30, required=True)
+    kelurahan = forms.CharField(label='Kelurahan/Desa', max_length=30, required=True)
+    kecamatan = forms.CharField(label='Kecamatan',max_length=30, required=True)
+    kabupaten_kota = forms.CharField(label='Kabupaten/Kota',max_length=30, required=True)
+    provinsi = forms.CharField(label='Provinsi', max_length=30, required=True)
--- hotel/urls.py
+++ hotel/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import create_hotel_view
+from .views import create_hotel_view, list_hotel_view, update_hotel_view
 
 urlpatterns = [
-    path('create/', create_hotel_view, name='create_hotel')
+    path('', list_hotel_view, name='list_hotel'),
+    path('create/', create_hotel_view, name='create_hotel'),
+    path('update/<str:kode>', update_hotel_view, name='update_hotel')
 
 ]
